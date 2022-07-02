79f9f89b369551d55bbf38d253946abf5aa6c4e8
RichLii <lichi0623@gmail.com>
2022-07-02 21:56:09 +0800

    新增一鍵自動部署 shell

--- auto_deploy.sh
+++ auto_deploy.sh
@@ -1,11 +1,7 @@
 # npm build 部分
 npm run build
 
-cd dist
-
-cp /dist/index.html /public/404.html
-
-cd ..
+copy %cd%\dist\index.html %cd%\public\404.html /y
 
 npm run build
 
@@ -18,7 +14,7 @@ git init
 git checkout -b gh-pages
 
 # 只需要修改這裡為你的 git repo
-git remote add origin git@github.com:RichLii/test.git
+git remote add origin git@github.com:RichLii/Vue-Books-List.git
 
 git add .
 
